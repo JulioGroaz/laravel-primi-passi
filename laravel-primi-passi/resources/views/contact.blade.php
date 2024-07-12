e20e4770478b853ced647a39398af0a54ac5f911
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-12 16:49:51 +0200

    Ho creato la navbar

--- laravel-primi-passi/resources/views/contact.blade.php
+++ laravel-primi-passi/resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Home</title>
+
+    <title>Contact</title>
 
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -10,15 +10,16 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Home Page</h1>
-                <p>Welcome to the Home Page.</p>
+                <h1>Contact Page</h1>
+                <p>Get in touch with us through this page.</p>
             </div>
         </div>
     </div>
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-
 
